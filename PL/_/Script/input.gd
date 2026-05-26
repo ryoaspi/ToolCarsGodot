acfc8e6227f25a4a0d54fa5012116ce4bc92887a
Nos_ave <nos@ik.me>
2026-05-26 16:23:52 +0200

    [DEV] Add keyboard event input

---
+++ PL/_/Script/input.gd
@@ -0,0 +1,24 @@
+extends Node
+
+
+@export var name_for_up: String = "Up"
+@export var name_for_down: String = "Down"
+@export var name_for_left: String = "Left"
+@export var name_for_right: String = "Right"
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if name_for_up == event.as_text_key_label():
+			print("I'm UP key")
+
+		elif name_for_down == event.as_text_key_label():
+			print("I'm DOWN Key")
+
+		elif name_for_left == event.as_text_key_label():
+			print("I'm LEFT Key")
+
+		elif name_for_right == event.as_text_key_label():
+			print("I'm RIGHT Key")
+
+		else:
+			print("I don`t arrow key !")
