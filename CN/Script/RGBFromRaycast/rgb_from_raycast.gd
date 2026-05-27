5a27ec35d8089ca0677125ddb5f8c287fa68bdc2
Carlier Nicolas <n.carlier6000@gmail.com>
2026-05-27 14:14:35 +0200

    [Nicolas]Read Color on Texture with raycast, signal create and public method to call

--- CN/Script/RGBFromRaycast/rgb_from_raycast.gd
+++ CN/Script/RGBFromRaycast/rgb_from_raycast.gd
@@ -5,8 +5,10 @@ extends Node3D
 @onready var raycast: RayCast3D = $MoveRaycast/RayCast3D
 var last_object_hit : Node3D
 var _image : Image
+var pixel_position : Vector2i
 
-@export var debug : Vector3i
+signal on_rgba_from_raycast(rgba : Vector4i)
+
 
 
 func _physics_process(delta: float) -> void:
@@ -19,12 +21,13 @@ func _physics_process(delta: float) -> void:
 		var mesh_size = object.mesh.size
 		mesh_size = Vector3(mesh_size.x,0,mesh_size.y)
 		var corner_position = object.global_position - mesh_size * 0.5
-		var hit_position = raycast.global_position - corner_position
+		var hit_position = raycast.get_collision_point() - corner_position
 		var pixelX : int = remap(hit_position.x,0,mesh_size.x,0,1) * 512
 		var pixelY : int = remap(hit_position.z,0,mesh_size.z,0,1) * 512
-		var pixel_position = Vector2i(pixelX,pixelY)
-		debug = _get_RGB_from_image(pixel_position)
-		print (debug)
+		pixel_position = Vector2i(pixelX,pixelY)
+		
+		var color_v4i : Vector4i = _get_RGBA_from_image(pixel_position)
+		on_rgba_from_raycast.emit(color_v4i)
 
 func _result_change(new_object: Node3D) -> bool:
 	if not last_object_hit:
@@ -34,15 +37,24 @@ func _result_change(new_object: Node3D) -> bool:
 		last_object_hit = new_object
 		return true
 	return false
-
-func _get_RGB_from_image(pixel : Vector2i) -> Vector3i:
-	var color : Color = Color.BLACK
-	if _image:
-		color = _image.get_pixelv(pixel)
-	return Vector3i(remap(color.r,0,1,0,255),remap(color.g,0,1,0,255),remap(color.b,0,1,0,255))
-
+	
 
 func _set_image_from_object(object : Node3D) -> void:
-	var texture: Texture2D = object.get_surface_override_material(0).albedo_texture
+	var mesh_instance := object as MeshInstance3D
+	
+	var texture = mesh_instance.get_active_material(0).albedo_texture
+	
 	if texture:
 		_image = texture.get_image()
+
+func _get_RGBA_from_image(pixel : Vector2i) -> Vector4i:
+	var color : Color = Color.from_rgba8(0,0,0,0)
+	if _image:
+		color = _image.get_pixelv(pixel)
+	return Vector4i(remap(color.r,0,1,0,255),
+	remap(color.g,0,1,0,255),
+	remap(color.b,0,1,0,255),
+	remap(color.a,0,1,0,255))
+
+func get_RGBA_from_raycast() -> Vector4i:
+	return _get_RGBA_from_image(pixel_position)
