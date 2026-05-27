05a41f08abc482c2fa7ac21ec8527b8a4cabe414
ShizuK8 <jurgen20112024@gmail.com>
2026-05-27 09:13:00 +0200

    [Dev] Jürgen - JumpScript4Fun

--- EJ/Scripts/controler_keys.gd
+++ EJ/Scripts/controler_keys.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
-@export var SPEED : float = 4
-@export var TURN_SPEED : float = 3.0
+@export var speed : float = 4
+@export var turn_speed : float = 3.0
+@export var jump_impulse = 5
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -14,17 +15,20 @@ func _physics_process(delta: float) -> void:
 
 #Left/Right
 	if abs(left_right) > 0.1:
-		rotate_y(-left_right * TURN_SPEED * delta)
+		rotate_y(-left_right * turn_speed * delta)
 #Forward/Backward
 	var direction = Vector3.ZERO
 	if abs(forward_back) > 0.1:
 		direction = -transform.basis.z * forward_back
 
 	if direction.length() > 0:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
-		velocity.z = move_toward(velocity.z, 0, SPEED * delta)
+		velocity.x = move_toward(velocity.x, 0, speed * delta)
+		velocity.z = move_toward(velocity.z, 0, speed * delta)
+	#Jump4Fun
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_impulse
 
 	move_and_slide()
