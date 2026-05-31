567a0c8164155926ff6f4071f569ee3ddbc8267d
Éloi Strée <eloistree@gmail.com>
2026-05-31 18:43:25 +0200

    Create hello_world.gd

---
+++ _Main/Gist/eji/hello_world.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+func _ready() -> void:
+	pass 
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_car_received(car_node: Node):
+	print(car_node)
+	print("Hello Mod, Salut Richard")
